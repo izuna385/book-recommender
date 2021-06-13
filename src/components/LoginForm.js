1f92e9a5d52f955d38da43d288daccba410ac961
izuna385 <izuna385@gmail.com>
2021-06-13 14:17:14 +0900

    formをFormikに変更. Hooksの書き方のドキュメントが充実しているため。

---
+++ src/components/LoginForm.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+
+const loginSchema = Yup.object().shape({
+  password: Yup.string()
+    .min(8, "Too Short!")
+    .max(50, "Too Long!")
+    .required("Required"),
+  email: Yup.string().email("Invalid email").required("Required")
+});
+
+const LoginForm = () => {
+  const handleSubmit = (values, { setSubmitting }) => {
+    setTimeout(() => {
+      alert(JSON.stringify(values, null, 2));
+      setSubmitting(false);
+    }, 400);
+  };
+
+    return (
+      <>
+        <h1>Login</h1>
+        <Formik
+          initialValues={{ email: "", password: "" }}
+          validationSchema={loginSchema}
+          onSubmit={handleSubmit}
+        >
+          {({ isSubmitting }) => {
+            return (
+              <Form>
+                <label>
+                  Email: <Field type="email" name="email" />
+                  <ErrorMessage name="email" component="div" />
+                </label>
+                <label>
+                  Password:
+                  <Field type="password" name="password" />
+                  <ErrorMessage name="password" component="div" />
+                </label>
+                <button type="submit" disabled={isSubmitting}>
+                  Submit
+                </button>
+              </Form>
+            );
+          }}
+        </Formik>
+      </>
+    );
+  }
+
+export default LoginForm;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,22 +1,11 @@
 import { useForm } from 'react-hook-form'
-
+import LoginForm from './../components/LoginForm'
 
 const Login = () => {
-  const { handleSubmit, register, reset } = useForm()
-  const submit = (data) => {
-    // 送信ボタンを押したら入力欄をブランク状態に戻す
-    reset({ message: '' })
-  }
+
   return (
     <div>
-      <form onSubmit={handleSubmit(submit)}>
-        <textarea
-          placeholder="メッセージを入力"
-          // https://stackoverflow.com/questions/66927051/getting-uncaught-typeerror-path-split-is-not-a-function-in-react
-          {...register('message', { required: true })}
-        />
-        <button>送信</button>
-      </form>
+      <LoginForm />
     </div>
   )
 }
